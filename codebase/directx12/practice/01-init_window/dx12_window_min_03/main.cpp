47f91915f6019d52cbb1926fe9670a013013c986
Mike Lee <rockespecialee@gmail.com>
2023-09-22 00:43:59 -0700

    100 lines achieved

--- codebase/directx12/practice/01-init_window/dx12_window_min_03/main.cpp
+++ codebase/directx12/practice/01-init_window/dx12_window_min_03/main.cpp
@@ -14,7 +14,6 @@ ComPtr<ID3D12PipelineState> m_pipelineState;
 ComPtr<ID3D12GraphicsCommandList> m_commandList;
 UINT m_rtvDescriptorSize;
 UINT m_frameIndex;
-
 UINT m_width = 1280;
 UINT m_height = 720;
 static HWND m_hwnd;
@@ -22,7 +21,6 @@ static HWND m_hwnd;
 void LoadPipeline() {
     ComPtr<IDXGIAdapter1> hardwareAdapter;
     D3D12CreateDevice(hardwareAdapter.Get(), D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&m_device));
-
     D3D12_COMMAND_QUEUE_DESC queueDesc = {};
     m_device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&m_commandQueue));
 
@@ -58,34 +56,26 @@ void LoadPipeline() {
     m_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_commandAllocator));
     m_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_commandAllocator.Get(), nullptr, IID_PPV_ARGS(&m_commandList));
 }
-
 void PopulateCommandList() {
     CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart(), m_frameIndex, m_rtvDescriptorSize);
     const float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
     m_commandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
     m_commandList->Close();
 }
-
 void OnRender() {
     PopulateCommandList();
     ID3D12CommandList* ppCommandLists[] = { m_commandList.Get() };
     m_commandQueue->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
     m_swapChain->Present(1, 0);
 }
-
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
-        case WM_CLOSE:
-            PostQuitMessage(0);
-            break;
-        case WM_PAINT:
-            OnRender();
-        default:
-            return DefWindowProc(hWnd, message, wParam, lParam);
+        case WM_CLOSE: PostQuitMessage(0); break;
+        case WM_PAINT: OnRender();
+        default: return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
 }
-
 _Use_decl_annotations_
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPSTR cmdLine, int showCmd) {
     WNDCLASSEX wc = { 0 };
@@ -97,17 +87,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPSTR cmdLine, i
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
     wc.lpszClassName = L"DX12 Window Init";
     if (!RegisterClassEx(&wc)) { return 1; }
-
     RECT windowRect = { 0, 0, static_cast<LONG>(m_width), static_cast<LONG>(m_height) };
     AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
-
-    m_hwnd = CreateWindow(wc.lpszClassName, L"Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT,
-        windowRect.right - windowRect.left, windowRect.bottom - windowRect.top, 0, 0, hInstance, NULL);
+    m_hwnd = CreateWindow(wc.lpszClassName, L"Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, windowRect.right - windowRect.left, windowRect.bottom - windowRect.top, 0, 0, hInstance, NULL);
 
     if (!m_hwnd) { return 2; }
-
     LoadPipeline();
-
     MSG msg = {};
     while (GetMessage(&msg, NULL, 0, 0) > 0) { DispatchMessage(&msg); }
 
