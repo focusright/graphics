1578843157fbb20b78c75f92d9011405cfd54ae9
Mike Lee <rockespecialee@gmail.com>
2023-09-22 00:39:53 -0700

    115 lines achieved

---
+++ codebase/directx12/practice/01-init_window/dx12_window_min_03/main.cpp
@@ -0,0 +1,115 @@
+#include "d3dx12.h"
+#include <dxgi1_6.h>
+#include <stdexcept>
+#define FRAMECOUNT 2
+using Microsoft::WRL::ComPtr;
+
+ComPtr<IDXGISwapChain3> m_swapChain;
+ComPtr<ID3D12Device> m_device;
+ComPtr<ID3D12Resource> m_renderTargets[FRAMECOUNT];
+ComPtr<ID3D12CommandAllocator> m_commandAllocator;
+ComPtr<ID3D12CommandQueue> m_commandQueue;
+ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
+ComPtr<ID3D12PipelineState> m_pipelineState;
+ComPtr<ID3D12GraphicsCommandList> m_commandList;
+UINT m_rtvDescriptorSize;
+UINT m_frameIndex;
+
+UINT m_width = 1280;
+UINT m_height = 720;
+static HWND m_hwnd;
+
+void LoadPipeline() {
+    ComPtr<IDXGIAdapter1> hardwareAdapter;
+    D3D12CreateDevice(hardwareAdapter.Get(), D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&m_device));
+
+    D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+    m_device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&m_commandQueue));
+
+    DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+    swapChainDesc.BufferCount = FRAMECOUNT;
+    swapChainDesc.Width = m_width;
+    swapChainDesc.Height = m_height;
+    swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+    swapChainDesc.SampleDesc.Count = 1;
+
+    ComPtr<IDXGIFactory4> factory;
+    CreateDXGIFactory2(0, IID_PPV_ARGS(&factory));
+    ComPtr<IDXGISwapChain1> swapChain;
+    factory->CreateSwapChainForHwnd(m_commandQueue.Get(), m_hwnd, &swapChainDesc, nullptr, nullptr, &swapChain);
+    factory->MakeWindowAssociation(m_hwnd, DXGI_MWA_NO_ALT_ENTER);
+    swapChain.As(&m_swapChain);
+    m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
+
+    D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+    rtvHeapDesc.NumDescriptors = FRAMECOUNT;
+    rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+    m_device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&m_rtvHeap));
+    m_rtvDescriptorSize = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+
+    CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart());
+    for (UINT n = 0; n < FRAMECOUNT; n++) {
+        m_swapChain->GetBuffer(n, IID_PPV_ARGS(&m_renderTargets[n]));
+        m_device->CreateRenderTargetView(m_renderTargets[n].Get(), nullptr, rtvHandle);
+        rtvHandle.Offset(1, m_rtvDescriptorSize);
+    }
+    m_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_commandAllocator));
+    m_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_commandAllocator.Get(), nullptr, IID_PPV_ARGS(&m_commandList));
+}
+
+void PopulateCommandList() {
+    CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart(), m_frameIndex, m_rtvDescriptorSize);
+    const float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
+    m_commandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
+    m_commandList->Close();
+}
+
+void OnRender() {
+    PopulateCommandList();
+    ID3D12CommandList* ppCommandLists[] = { m_commandList.Get() };
+    m_commandQueue->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
+    m_swapChain->Present(1, 0);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+        case WM_CLOSE:
+            PostQuitMessage(0);
+            break;
+        case WM_PAINT:
+            OnRender();
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+_Use_decl_annotations_
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPSTR cmdLine, int showCmd) {
+    WNDCLASSEX wc = { 0 };
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.hbrBackground = (HBRUSH)(COLOR_BACKGROUND);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.lpszClassName = L"DX12 Window Init";
+    if (!RegisterClassEx(&wc)) { return 1; }
+
+    RECT windowRect = { 0, 0, static_cast<LONG>(m_width), static_cast<LONG>(m_height) };
+    AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+    m_hwnd = CreateWindow(wc.lpszClassName, L"Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT,
+        windowRect.right - windowRect.left, windowRect.bottom - windowRect.top, 0, 0, hInstance, NULL);
+
+    if (!m_hwnd) { return 2; }
+
+    LoadPipeline();
+
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0) > 0) { DispatchMessage(&msg); }
+
+    return 0;
+}
