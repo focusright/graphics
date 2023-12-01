a0252a339ce1a94438636f97996c49d07ee539ae
Mike Lee <rockespecialee@gmail.com>
2023-11-30 20:55:32 -0800

    add doslike

---
+++ codebase/doslike/sources/graphics.c
@@ -0,0 +1,22 @@
+//tcc\tcc source\graphics.c source\dos.c
+//wasm\node wasm\wajicup.js source\graphics.c source\dos.c graphics.html
+
+#include <stdlib.h>
+#include "dos.h"
+
+int main(int argc, char* argv[]) {
+    setvideomode(videomode_320x200);
+
+    while(!shuttingdown()) {
+        waitvbl();
+        for(int i=0; i<50; ++i) {
+            setcolor(rand() % 256);
+            line(rand() % 320, rand() % 200, rand() % 320, rand() % 200);
+            setcolor(rand() % 256);
+            fillcircle(rand() % 320, rand() % 200, rand() % 30);
+            setcolor(rand() % 256);
+            circle(rand() % 320, rand() % 200, rand() % 30);
+        }
+    }
+    return 0;
+}
