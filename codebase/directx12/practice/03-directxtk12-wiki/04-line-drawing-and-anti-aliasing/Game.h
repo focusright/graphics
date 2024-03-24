52ee3287f639dc0fb28c543498342fedde1a980a
Mike Lee <rockesepcialee@gmail.com>
2024-03-24 15:55:53 -0700

    add 04-line-drawing-and-anti-aliasing, rotating + anti-aliased grid

---
+++ codebase/directx12/practice/03-directxtk12-wiki/04-line-drawing-and-anti-aliasing/Game.h
@@ -0,0 +1,80 @@
+//
+// Game.h
+//
+
+#pragma once
+
+#include "DeviceResources.h"
+#include "StepTimer.h"
+
+
+// A basic game implementation that creates a D3D12 device and
+// provides a game loop.
+class Game final : public DX::IDeviceNotify
+{
+public:
+
+    Game() noexcept(false);
+    ~Game() = default;
+
+    Game(Game&&) = default;
+    Game& operator= (Game&&) = default;
+
+    Game(Game const&) = delete;
+    Game& operator= (Game const&) = delete;
+
+    // Initialization and management
+    void Initialize(::IUnknown* window, int width, int height, DXGI_MODE_ROTATION rotation);
+
+    // Basic game loop
+    void Tick();
+
+    // IDeviceNotify
+    void OnDeviceLost() override;
+    void OnDeviceRestored() override;
+
+    // Messages
+    void OnActivated();
+    void OnDeactivated();
+    void OnSuspending();
+    void OnResuming();
+    void OnDisplayChange();
+    void OnWindowSizeChanged(int width, int height, DXGI_MODE_ROTATION rotation);
+    void ValidateDevice();
+
+    // Properties
+    void GetDefaultSize( int& width, int& height ) const noexcept;
+
+private:
+
+    void Update(DX::StepTimer const& timer);
+    void Render();
+
+    void Clear();
+
+    void CreateDeviceDependentResources();
+    void CreateWindowSizeDependentResources();
+
+    // Device resources.
+    std::unique_ptr<DX::DeviceResources>        m_deviceResources;
+
+    // Rendering loop timer.
+    DX::StepTimer                               m_timer;
+
+    // If using the DirectX Tool Kit for DX12, uncomment this line:
+    std::unique_ptr<DirectX::GraphicsMemory> m_graphicsMemory;
+
+    std::unique_ptr<DirectX::BasicEffect> m_effect;
+
+    std::unique_ptr<DirectX::PrimitiveBatch<
+        DirectX::VertexPositionColor>> m_batch;
+
+    DirectX::SimpleMath::Matrix m_world;
+    DirectX::SimpleMath::Matrix m_view;
+    DirectX::SimpleMath::Matrix m_proj;
+
+    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_rtvDescriptorHeap;
+    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_dsvDescriptorHeap;
+    Microsoft::WRL::ComPtr<ID3D12Resource> m_depthStencil;
+    Microsoft::WRL::ComPtr<ID3D12Resource> m_offscreenRenderTarget;
+};
