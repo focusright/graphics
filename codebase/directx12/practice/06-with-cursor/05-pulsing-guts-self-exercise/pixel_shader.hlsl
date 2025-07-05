1e049422daa3687c0777c72b5754381b2135ebbb
Mike Lee <rockespecialee@gmail.com>
2025-07-05 09:54:14 -0700

    breakdown more

--- codebase/directx12/practice/06-with-cursor/05-pulsing-guts-self-exercise/pixel_shader.hlsl
+++ codebase/directx12/practice/06-with-cursor/05-pulsing-guts-self-exercise/pixel_shader.hlsl
@@ -23,8 +23,9 @@ float4 PSMain(PSInput input) : SV_TARGET {
     fragCoord.y = iResolution.y - fragCoord.y;
     // Normalized, centered pixel coordinates (aspect-correct)
     float2 normalizedPos = (fragCoord - 0.5f * iResolution) / iResolution.y;
-    float3 finalColor = float3(0.0f, 0.0f, 0.0f);
     float time = iTime;
+    
+    float3 finalColor = float3(0.0f, 0.0f, 0.0f);
     float2 noiseOffset = float2(0.0f, 0.0f);
     float2 warpedPos = float2(0.0f, 0.0f);
     float2 iterPos = normalizedPos;
@@ -32,15 +33,24 @@ float4 PSMain(PSInput input) : SV_TARGET {
     float scale = 12.0f;
     float accum = 0.0f;
     float2x2 rotationMatrix = rotate2D(5.0f);
+    
+    float cycle = 0.0f;
+    const float SPEED = 4.0f;
+    
     [loop]
-    for (float iter = 0.0f; iter < 20.0f; iter += 1.0f) {
+    for (float iteration = 0.0f; iteration < 20.0f; iteration += 1.0f) {
         iterPos = mul(iterPos, rotationMatrix);
         noiseOffset = mul(noiseOffset, rotationMatrix);
-        warpedPos = iterPos * scale + time * 4.0f + sin(time * 4.0f - distFromCenter * 6.0f) * 0.8f + iter + noiseOffset;
+        cycle = sin(time * SPEED - distFromCenter * 6.0f);
+        warpedPos = iterPos * scale + time * SPEED + cycle * 0.8f + iteration + noiseOffset;
         accum += dot(cos(warpedPos) / scale, float2(0.2f, 0.2f));
         noiseOffset -= sin(warpedPos);
         scale *= 1.2f;
     }
-    finalColor = float3(4.0f, 2.0f, 1.0f) * (accum + 0.2f) + accum + accum - distFromCenter;
+    
+    const float3 ORANGE = float3(4.0f, 2.0f, 1.0f);
+    const float CONTRAST = 2.0f;
+    
+    finalColor = ORANGE * (accum + 0.2f) + (accum * CONTRAST) - distFromCenter;
     return float4(finalColor.x, finalColor.y, finalColor.z, 1.0f);
 } 
