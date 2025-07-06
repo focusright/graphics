2aec6b2dc18b27f2364b339670280c7a283cff04
Mike Lee <rockespecialee@gmail.com>
2025-07-06 03:04:08 -0700

    add comments to pixel_shader_01.hlsl

--- codebase/directx12/practice/06-with-cursor/05-pulsing-guts-self-exercise/pixel_shader_01.hlsl
+++ codebase/directx12/practice/06-with-cursor/05-pulsing-guts-self-exercise/pixel_shader_01.hlsl
@@ -16,11 +16,23 @@ float4 PSMain(PSInput input) : SV_TARGET {
     // Flip Y to match ShaderToy's bottom-left origin
     fragCoord.y = iResolution.y - fragCoord.y;
     const float2 normalizedPos = (fragCoord - 0.5f * iResolution) / iResolution.y;
-    //squared distance from the center (0,0) to the current pixel
+    //distFromCenter = squared distance from the center (0,0) to the current pixel
     const float distFromCenter = dot(normalizedPos, normalizedPos);
+    
+    //The color range is clamped to the valid range of [0.0, 1.0]
+    //Without the dimming factor, one of the following will work:
+    
+    //const float3 ORANGE = float3(1.0f, 0.5f, 0.25f);
+    //const float3 ORANGE = float3(4.0f, 2.0f, 1.0f) / 4.0f; // = (1.0f, 0.5f, 0.25f)
+    //const float3 ORANGE = normalize(float3(4.0f, 2.0f, 1.0f));
+    
     const float3 ORANGE = float3(4.0f, 2.0f, 1.0f);
+    
     const float DIM_FACTOR = 0.2f;
-    const float3 finalColor = ORANGE * DIM_FACTOR - distFromCenter;
+    const float3 finalColor = (ORANGE * DIM_FACTOR) - distFromCenter;
+    
+    //Without the dimming factor:
+    //const float3 finalColor = ORANGE - distFromCenter;
     
     return float4(finalColor.x, finalColor.y, finalColor.z, 1.0f);
 } 
