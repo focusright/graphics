c31e17dc6356951b4c773932772aebeb35678fa9
Mike Lee <rockespecialee@gmail.com>
2025-07-06 03:31:45 -0700

    simplify pixel_shader_02.hlsl

--- codebase/directx12/practice/06-with-cursor/05-pulsing-guts-self-exercise/pixel_shader_02.hlsl
+++ codebase/directx12/practice/06-with-cursor/05-pulsing-guts-self-exercise/pixel_shader_02.hlsl
@@ -11,12 +11,6 @@ struct PSInput {
     float4 position : SV_POSITION;
 };
 
-float2x2 rotate2D(float angle) {
-    float c = cos(angle);
-    float s = sin(angle);
-    return float2x2(c, s, -s, c);
-}
-
 float4 PSMain(PSInput input) : SV_TARGET {
     float2 fragCoord = input.position.xy;
     fragCoord.y = iResolution.y - fragCoord.y;
@@ -24,34 +18,25 @@ float4 PSMain(PSInput input) : SV_TARGET {
     float time = iTime;
     
     float3 finalColor = float3(0.0f, 0.0f, 0.0f);
-    float2 noiseOffset = float2(0.0f, 0.0f);
     float2 warpedPosition = float2(0.0f, 0.0f);
-    float2 spiralOut = float2(0.0f, 0.0f);
-    float2 iterationPosition = normalizedPos;
-    float distFromCenter = dot(iterationPosition, iterationPosition);
+    float distFromCenter = dot(normalizedPos, normalizedPos);
     float scale = 12.0f;
     float accum = 0.0f;
-    float2x2 rotationMatrix = rotate2D(5.0f);
     
     const float SPEED = 4.0f;
-    const float PULSE_INTENSITY = 0.8f;
     const float CRANK = time * SPEED;
-    const float CYCLE = sin(CRANK - distFromCenter * 6.0f) * PULSE_INTENSITY;
+    const float CYCLE = sin(CRANK - distFromCenter * 6.0f);
     
     [loop]
     for (float i = 0.0f; i < 1.0f; i += 1.0f) {
-        iterationPosition = mul(iterationPosition, rotationMatrix);
-        //noiseOffset = mul(noiseOffset, rotationMatrix);
-        //spiralOut = iterationPosition * scale;
-        warpedPosition = spiralOut + CRANK + CYCLE + i + noiseOffset;
+        warpedPosition =  CRANK + CYCLE + i;
         accum += dot(cos(warpedPosition) / scale, float2(0.2f, 0.2f));
-        //noiseOffset -= sin(warpedPosition);
-        //scale *= 1.2f;
     }
     
     const float3 ORANGE = float3(4.0f, 2.0f, 1.0f);
     const float CONTRAST = 2.0f;
+    const float DIM_FACTOR = 0.2f;
     
-    finalColor = ORANGE * (accum + 0.2f) + (accum * CONTRAST) - distFromCenter;
+    finalColor = ORANGE * (accum + DIM_FACTOR) + (accum * CONTRAST) - distFromCenter;
     return float4(finalColor.x, finalColor.y, finalColor.z, 1.0f);
 }
